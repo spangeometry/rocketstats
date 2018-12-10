50502e654c4a705c4a0da8dd722ffbc4e5e2e7a6
Sam <serioussamix@gmail.com>
2018-12-10 06:05:50 -0800

    Barebones UI implemented

--- rocketstats/rocketstats/PersonalViewController.swift
+++ rocketstats/rocketstats/PersonalViewController.swift
@@ -15,16 +15,25 @@ class PersonalViewController: UIViewController {
     
     var userPlatform: String = "ps"
     var userID: String = "serioussamix"
+    @IBOutlet weak var statShots: UILabel!
+    @IBOutlet weak var statSaves: UILabel!
+    @IBOutlet weak var userIDLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.userIDLabel.text = userID
         // Do any additional setup after loading the view, typically from a nib.
         Alamofire.request(generatePersonalURL(platform: userPlatform, userID: userID)).responseJSON {
             (responseData) -> Void in
             if((responseData.result.value) != nil) {
                 let swiftyJSONVar = JSON(responseData.result.value!)
                 //print(swiftyJSONVar)
-                if let testValue = swiftyJSONVar["data"]["performance"][0]["statistic"]["title"].string {
+                if let testValue = swiftyJSONVar["data"]["performance"][5]["statistic"]["value"].string {
+                    self.statShots.text = testValue
+                    print(testValue)
+                }
+                if let testValue = swiftyJSONVar["data"]["performance"][4]["statistic"]["value"].string {
+                    self.statSaves.text = testValue
                     print(testValue)
                 }
             }
