5a4f509cac4ed604f8b3769db115b976cdf645d0
Sam <serioussamix@gmail.com>
2018-12-11 02:45:13 -0800

    Pre-tab work completed

--- rocketstats/rocketstats/PersonalViewController.swift
+++ rocketstats/rocketstats/PersonalViewController.swift
@@ -13,24 +13,36 @@ import SwiftyJSON
 
 class PersonalViewController: UIViewController {
     
-    var userPlatform: String = "ps"
-    var userID: String = "serioussamix"
+    var userPlatform: String = ""
+    var userID: String = ""
     @IBOutlet weak var statShots: UILabel!
     @IBOutlet weak var statSaves: UILabel!
     @IBOutlet weak var userIDLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.userIDLabel.text = userID
+        fetchSettings()
+        loadPersonalJSON(platform: userPlatform, userID: userID)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    func loadPersonalJSON(platform: String, userID: String) {
         Alamofire.request(generatePersonalURL(platform: userPlatform, userID: userID)).responseJSON {
             (responseData) -> Void in
             if((responseData.result.value) != nil) {
                 let swiftyJSONVar = JSON(responseData.result.value!)
-                //print(swiftyJSONVar)
+                print(swiftyJSONVar)
+                if swiftyJSONVar["success"].description == "false" {
+                    print("no success")
+                    self.userIDLabel.text = "no success"
+                    return
+                }
                 if let testValue = swiftyJSONVar["data"]["performance"][5]["statistic"]["value"].string {
                     self.statShots.text = testValue
                     print(testValue)
+                    self.userIDLabel.text = "success"
                 }
                 if let testValue = swiftyJSONVar["data"]["performance"][4]["statistic"]["value"].string {
                     self.statSaves.text = testValue
@@ -38,7 +50,17 @@ class PersonalViewController: UIViewController {
                 }
             }
         }
+    }
+    
+    func fetchSettings() {
+        let defaults = UserDefaults.standard
+        let appDefaults = ["settingsUsername": "serioussamix","settingsPlatform": "ps"]
+        defaults.register(defaults: appDefaults)
+        defaults.synchronize()
         
+        userID = defaults.string(forKey: "settingsUsername")!
+        self.userIDLabel.text = userID
+        userPlatform = defaults.string(forKey: "settingsPlatform")!
     }
     
     func fetchPersonalData(completion: @escaping ([String:Any]?, Error?) -> Void) {
